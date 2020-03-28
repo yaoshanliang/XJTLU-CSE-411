cc49a63096a7cc091d20ba0819593e0dfde90867
iat <i@iat.net.cn>
2020-03-28 17:04:02 +0800

    update

--- fruitWebsite/js/fruits.js
+++ fruitWebsite/js/fruits.js
@@ -1,27 +1,71 @@
 let fruits = [
-    {
-        name: 'apple',
-        image: 'apple.jpg',
-        desc: 'Green	Medium	Crispy'
-    },
-    {
-        name: 'orange',
-        image: 'orange.jpg',
-        desc: 'Orange	Large	Sweet'
-    },
-    {
-        name: 'lemon',
-        image: 'lemon.jpg',
-        desc: 'Yellow	Small	Tangy'
-    },
-    {
-        name: 'banana',
-        image: 'banana.jpg',
-        desc: 'Yellow	Large	Soft'
-    },
-    {
-        name: 'more',
-        image: 'more.jpg',
-        desc: 'Colorful'
-    }
+  {
+    name: "Apple",
+    image: "apple.jpg",
+    desc: "Green	Medium	Crispy"
+  },
+  {
+    name: "Orange",
+    image: "orange.jpg",
+    desc: "Orange	Large	Sweet"
+  },
+  {
+    name: "Lemon",
+    image: "lemon.jpg",
+    desc: "Yellow	Small	Tangy"
+  },
+  {
+    name: "Banana",
+    image: "banana.jpg",
+    desc: "Yellow	Large	Soft"
+  },
+  {
+    name: "More",
+    image: "more.jpg",
+    desc: "Colorful"
+  }
 ];
+
+function menu() {
+  let menu = "";
+  let html = "";
+
+  fruits.map(item => {
+    console.log(item);
+    menu +=
+      "<li>" +
+      '<a href="./' +
+      item.name +
+      '.html">' +
+      item.name +
+      "</a>" +
+      "</li>";
+
+    html +=
+      '<div class="col-md-20">' +
+      '<div class="thumbnail">' +
+      '<div class="image view view-first">' +
+      '<a href="./' +
+      item.name +
+      '.html"> <img class="image-item" src=./images/' +
+      item.image +
+      ' alt="' +
+      item.name +
+      '"></a>' +
+      '<div class="mask">' +
+      "<p>Your Text</p>" +
+      '<div class="tools tools-bottom">' +
+      "</div>" +
+      "</div>" +
+      "</div >" +
+      '<div class="caption">' +
+      "<p>" +
+      item.desc +
+      "</p>" +
+      "</div>" +
+      "</div >" +
+      "</div >";
+  });
+  document.getElementById("menu").insertAdjacentHTML("afterend", menu);
+  document.getElementById("list").innerHTML = html;
+}
