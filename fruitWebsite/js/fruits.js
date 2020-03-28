7d740be1d5a096d615619a8f8bf18eccbf8feb85
iat <i@iat.net.cn>
2020-03-28 16:54:16 +0800

    update

---
+++ fruitWebsite/js/fruits.js
@@ -0,0 +1,27 @@
+let fruits = [
+    {
+        name: 'apple',
+        image: 'apple.jpg',
+        desc: 'Green	Medium	Crispy'
+    },
+    {
+        name: 'orange',
+        image: 'orange.jpg',
+        desc: 'Orange	Large	Sweet'
+    },
+    {
+        name: 'lemon',
+        image: 'lemon.jpg',
+        desc: 'Yellow	Small	Tangy'
+    },
+    {
+        name: 'banana',
+        image: 'banana.jpg',
+        desc: 'Yellow	Large	Soft'
+    },
+    {
+        name: 'more',
+        image: 'more.jpg',
+        desc: 'Colorful'
+    }
+];
