e782c03c483736c9ad727834049af901a631b51e
iat <i@iat.net.cn>
2020-04-15 15:12:19 +0800

    add audios

--- CW1/js/data.js
+++ CW1/js/data.js
@@ -24,10 +24,40 @@ let songs = [
     path: "A Little Story.mp3"
   },
   {
-    name: "A Million Possibilities",
-    singer: "Christine Welch",
-    image: "A Million Possibilities.png",
-    path: "A Million Possibilities.mp3"
+    name: "The Show",
+    singer: "Lenka",
+    image: "The Show.png",
+    path: "The Show.mp3"
+  },
+  {
+    name: "Leave With A Song",
+    singer: "Sarah Connor",
+    image: "Leave With A Song.png",
+    path: "Leave With A Song.mp3"
+  },
+  {
+    name: "Rolling in the Deep",
+    singer: "Adele",
+    image: "Rolling in the Deep.png",
+    path: "Rolling in the Deep.mp3"
+  },
+  {
+    name: "I Knew You Were Trouble",
+    singer: "Party Hit Kings",
+    image: "I Knew You Were Trouble.png",
+    path: "I Knew You Were Trouble.mp3"
+  },
+  {
+    name: "You Belong With Me",
+    singer: "American Country Hits",
+    image: "You Belong With Me.png",
+    path: "You Belong With Me.mp3"
+  },
+  {
+    name: "Big Big World",
+    singer: "Emilia Rydberg",
+    image: "Big Big World.png",
+    path: "Big Big World.mp3"
   },
   
 ];
