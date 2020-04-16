bb0722a7dbcc7a525d54290a59cadddcb0e44582
iat <i@iat.net.cn>
2020-04-16 19:29:03 +0800

    update

--- CW1/js/data.js
+++ CW1/js/data.js
@@ -28,37 +28,7 @@ let songs = [
     singer: "Lenka",
     image: "The Show.png",
     path: "The Show.mp3"
-  },
-  {
-    name: "Leave With A Song",
-    singer: "Sarah Connor",
-    image: "Leave With A Song.png",
-    path: "Leave With A Song.mp3"
-  },
-  {
-    name: "Rolling in the Deep",
-    singer: "Adele",
-    image: "Rolling in the Deep.png",
-    path: "Rolling in the Deep.mp3"
-  },
-  {
-    name: "I Knew You Were Trouble",
-    singer: "Party Hit Kings",
-    image: "I Knew You Were Trouble.png",
-    path: "I Knew You Were Trouble.mp3"
-  },
-  {
-    name: "You Belong With Me",
-    singer: "American Country Hits",
-    image: "You Belong With Me.png",
-    path: "You Belong With Me.mp3"
-  },
-  {
-    name: "Big Big World",
-    singer: "Emilia Rydberg",
-    image: "Big Big World.png",
-    path: "Big Big World.mp3"
-  },
+  }
 ];
 
 let gallery = [
