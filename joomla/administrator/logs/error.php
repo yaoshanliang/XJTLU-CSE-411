57c9210ca7fbdbcd21e1e4e8b151a7cfe595861d
iat <i@iat.net.cn>
2020-05-12 22:27:46 +0800

    update

---
+++ joomla/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-05-12 07:47:11 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-05-12T07:47:11+00:00	INFO 127.0.0.1	joomlafailure	用户名和密码不匹配，或者你不是网站会员。
